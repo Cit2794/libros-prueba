61533c44ae5f1ffbb9c09ef519916a2016acd789
Cit2794 <43664395+Cit2794@users.noreply.github.com>
2018-09-30 23:08:14 -0500

    Add files via upload

--- addautor-pg.php
+++ addautor-pg.php
@@ -21,7 +21,7 @@ include ("conexion-pg.php");
     <nav aria-label="..." style="padding-left: 5%;">
       <ul class="pagination">
         <li class="page-item">
-          <a class="page-link" href="index.php" tabindex="-1">Agregar Libro</a>
+          <a class="page-link" href="index-pg.php" tabindex="-1">Agregar Libro</a>
         </li>
         <li class="page-item"><a class="page-link" href="select-pg.php">Libros</a></li>
         <li class="page-item active disabled">
@@ -34,7 +34,8 @@ include ("conexion-pg.php");
 			<form action="insert-autor-pg.php" method="POST">
 			  <div class="form-group">
 			    <label for="autor">Ingresa un autor nuevo:</label>
-			    <input type="text" class="form-control" name="autor" placeholder="Nombre del autor">
+			    <input type="text" class="form-control" name="autor" placeholder="Nombre del autor" data-validation="length alphanumeric required" data-validation-length="min4" data-validation-error-msg-container="#errorAutor">
+          <div id="errorAutor" class="alert alert-danger" role="alert"> Debe ingresar un autor válido.</div>
 			  </div>
 
 			  <button type="submit" class="btn btn-dark btn-lg btn-block" style="align-items: right;" value="enviar">Agregar</button>
@@ -42,10 +43,24 @@ include ("conexion-pg.php");
 		</div>
 
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <!-- JQuery Validator -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script>
+    <script>
+      $.validate({
+        validateOnBlur : true,
+        inlineErrorMessageCallback:  function($input, errorMessage, config) {
+           if (errorMessage) {
+            $('#errorAutor').show();
+           } else {
+            $('#errorAutor').hide();
+           }
+           return false;
+        }
+      });
+      $( document ).ready(function() {
+          $('#errorAutor').hide();
+      });
+    </script>
   </body>
 </html>
--- index.php
+++ index.php
@@ -7,28 +7,9 @@ include ("conexion-pg.php");
 	<head>
 		<!-- Boostrap 4.1 -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<!--
-    <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
-    <script src="js/jquery.form.js" type="text/javascript"></script>
-    <script src="js/jquery.validate.js" type="text/javascript"></script>
-    <script type="text/javascript">
-    $(document).ready(function(){
-        $("#contact-form").validate({
-            event: "blur",rules: {'titulo': "required"},
-            messages: {'titulo': "Debes ingresar un titulo"},
-            debug: true,errorElement: "label",
-            submitHandler: function(form){
-                $("#alert").show();
-                $("#alert").html("<img src='images/ajax-loader.gif' style='vertical-align:middle;margin:0 10px 0 0' /><strong>Enviando mensaje...</strong>");
-                setTimeout(function() {
-                    $('#alert').fadeOut('slow');
-                }, 5000);
-            }
-        });
-    });
-    </script> -->
+
 		<meta charset="UTF-8">
-		<title>Practica 01</title>
+		<title>Practica Biblioteca</title>
 	</head>
 	<body>
 
@@ -40,7 +21,7 @@ include ("conexion-pg.php");
 		<nav aria-label="..." style="padding-left: 5%;">
       <ul class="pagination">
         <li class="page-item active disabled">
-          <a class="page-link" href="index.php" tabindex="-1">Agregar Libro</a>
+          <a class="page-link" href="index-pg.php" tabindex="-1">Agregar Libro</a>
         </li>
         <li class="page-item"><a class="page-link" href="select-pg.php">Libros</a></li>
         <li class="page-item">
@@ -50,12 +31,12 @@ include ("conexion-pg.php");
     </nav>
 
 		<div class="container-fluid" style="padding: 5%;">
-    <!--  <div class="alert alert-success" id="alert" style="display: none;">&nbsp;</div> -->
 			<form action="insert-pg.php" method="POST" id="form-libro" name="form-libro">
 			  <div class="form-group">
 			    <label for="titulo">Título del libro:</label>
-			    <input type="text" class="form-control required" name="titulo" placeholder="Titulo" minlength=”4”>
-			  </div>
+			    <input type="text" class="form-control" name="titulo" placeholder="Titulo" data-validation="length alphanumeric required" data-validation-length="min1" data-validation-error-msg-container="#errorTitulo">
+          <div id="errorTitulo" class="alert alert-danger" role="alert"> Debe ingresar un título válido.</div>
+        </div>
 
 				<div class="form-group">
 			    <label for="exampleFormControlSelect1">Selecciona un autor</label>
@@ -77,10 +58,24 @@ include ("conexion-pg.php");
 			</form>
 		</div>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <!-- JQuery Validator -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script>
+    <script>
+      $.validate({
+        validateOnBlur : true,
+        inlineErrorMessageCallback:  function($input, errorMessage, config) {
+           if (errorMessage) {
+            $('#errorTitulo').show();
+           } else {
+            $('#errorTitulo').hide();
+           }
+           return false;
+        }
+      });
+      $( document ).ready(function() {
+          $('#errorTitulo').hide();
+      });
+    </script>
   </body>
 </html>
